e5eb3fca00d3fccda53f16fc26e685a2342f1d4e
Home <bing.yuan@163.com>
2014-08-26 23:07:45 +0800

    新增

---
+++ Wood/Controllers/UserAboutController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Wood.Controllers
+{
+    public class UserAboutController : Controller
+    {
+        //
+        // GET: /UserAbout/
+        public ActionResult About()
+        {
+            return View();
+        }
+	}
+}
--- Wood/Controllers/UserProductController.cs
+++ Wood/Controllers/UserProductController.cs
@@ -8,9 +8,20 @@ namespace Wood.Controllers
 {
     public class UserProductController : Controller
     {
-        //
-        // GET: /UserProduct/
+    
+        /// <summary>
+        /// 产品列表
+        /// </summary>
+        /// <returns></returns>
         public ActionResult ProductDetails()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 单个产品
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ProductInfo()
         {
             return View();
         }
