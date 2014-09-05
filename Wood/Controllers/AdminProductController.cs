139346c32282c0e6cfd35dd861b8e638fb47f910
Company <bing.yuan@163.com>
2014-09-05 17:14:36 +0800

    Change

---
+++ Wood/Controllers/AdminProductController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Wood.Controllers
+{
+    public class AdminProductController : Controller
+    {
+        //
+        // GET: /AdminProduct/
+        public ActionResult Product()
+        {
+            return View();
+        }
+
+        public ActionResult ProductType() 
+        {
+            return View();
+        }
+	}
+}
